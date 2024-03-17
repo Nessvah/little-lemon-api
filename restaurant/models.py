b0e51700bb49acd6d8a91d6336cae2c88c35f36a
Silvia Vanessa <info@silviavanessa.dev>
2024-03-17 18:03:34 +0000

    create models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    no_of_guests = models.SmallIntegerField()
+    booking_date = models.DateTimeField(db_index=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.booking_date}"
+
+
+class MenuTable(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
